a7f6eed05e25eb015b61d7300e3fc314d5f22c67
Gabriel Dornas <gabrielbdornas@gmail.com>
2021-04-11 15:27:23 -0300

    csv file importation working

--- app/controllers/importations_controller.rb
+++ app/controllers/importations_controller.rb
@@ -7,6 +7,13 @@ class ImportationsController < ApplicationController
   end
 
   def import
-
+    if params[:file].nil?
+      redirect_to importations_new_path
+    else
+      CSV.foreach(params[:file].path) do |row|
+        p row
+      end
+      redirect_to importations_new_path
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get "importations/new", to: "importations#new" #Importação arquivo .csv
-  get "importations/import", to: "importations#import" #Importação arquivo .csv
+  post "importations/import", to: "importations#import" #Importação arquivo .csv
   devise_for :users
   mount RailsAdmin::Engine => '/', as: 'rails_admin'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.htm
