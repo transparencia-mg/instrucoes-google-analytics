73ee6e3f512a040975ca73c3ac970fcd5e285052
Gabriel Dornas <gabrielbdornas@gmail.com>
2021-04-11 13:10:24 -0300

    rails_admin_template and RailsAdminTemplate name correction

--- config/application.rb
+++ config/application.rb
@@ -8,7 +8,7 @@ Bundler.require(*Rails.groups)
 
 ENV['RAILS_ADMIN_THEME'] = 'rollincode'
 
-module RailsAdminTemplate
+module TransparenciaGoogleAnalytics
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
