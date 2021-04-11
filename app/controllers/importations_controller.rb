bfe031450b1371cad880dd44f4757367a2215666
Gabriel Dornas <gabrielbdornas@gmail.com>
2021-04-11 15:47:25 -0300

    csv file importation working and csv split method to extract important url parts

--- app/controllers/importations_controller.rb
+++ app/controllers/importations_controller.rb
@@ -13,7 +13,7 @@ class ImportationsController < ApplicationController
       # csv_options = { col_sep: ',', quote_char: '"', headers: :first_row }
       # CSV.foreach(params[:file].path, csv_options) do |row|
       CSV.foreach(params[:file].path) do |row|
-        p row
+        puts "#{row[0]} | #{row[1]} | #{row[2]} | #{row[1].split('/').reject(&:blank?).count} | #{row[1].split('/').reject(&:blank?)}"
       end
       redirect_to importations_new_path
     end
