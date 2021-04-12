615fe325032eb7bd2aa13701da14c8d7fed1dfae
Gabriel Dornas <gabrielbdornas@gmail.com>
2021-04-12 15:57:44 -0300

    level and classification tables done - model translations and first roles - teste new tables

--- app/controllers/importations_controller.rb
+++ app/controllers/importations_controller.rb
@@ -15,9 +15,11 @@ class ImportationsController < ApplicationController
       # CSV.foreach(params[:file].path, csv_options) do |row|
       CSV.foreach(params[:file].path) do |row|
         # puts "#{row[0]} | #{row[1]} | #{row[2]} | #{row[1].split('/').reject(&:blank?).count} | #{row[1].split('/').reject(&:blank?)}"
+        url_array = row[1].split('/').reject(&:blank?)
+        raise
         file_path = params[:file].original_filename.split(' - ')[0].strip
         CSV.open(file_path, 'a+') do |csv|
-          csv << ["Categoria",
+          csv << [category(url_array),
                   row[0],
                   row[1],
                   row[2],
@@ -32,5 +34,29 @@ class ImportationsController < ApplicationController
       end
       redirect_to importations_new_path
     end
+  end
+
+  def category(url_array)
+    category_description = ''
+    if url_array.count = 1
+      level_one_url = url_descriptions(UrlClassification.where(level: Level.where(level:1)))
+      if level_one_url.include? url_array[0]
+        category_description =
+      end
+    elsif url_array.count = 2
+      level_two_url = url_descriptions(UrlClassification.where(level: Level.where(level:2)))
+    elsif url_array.count = 3
+      level_three_url = url_descriptions(UrlClassification.where(level: Level.where(level:3)))
+    else
+      category_description = 'Outros'
+    end
+  end
+
+  def url_descriptions(url_objects)
+    urls_array = []
+    url_objects.to_a.each do |url|
+      urls_array << url.name
+    end
+    urls_array
   end
 end
