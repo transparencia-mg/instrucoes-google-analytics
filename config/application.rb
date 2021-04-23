34367882d26fec49a80bc0570de645040d9c5b50
Gabriel Dornas <gabrielbdornas@gmail.com>
2021-04-23 19:10:49 -0300

    heroku alt of memory - https://github.com/activeadmin/activeadmin/issues/4118

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,7 @@ module TransparenciaGoogleAnalytics
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
+    config.middleware.delete "ActiveRecord::QueryCache"
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
