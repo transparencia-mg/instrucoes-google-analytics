e873f4952f1ab0736a9c23788e9491956d884e41
Gabriel Dornas <gabrielbdornas@gmail.com>
2023-01-24 13:20:08 -0300

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  get "importations/new", to: "importations#new" #Importação arquivo .csv
+  post "importations/import", to: "importations#import" #Importação arquivo .csv
+  get "export", to: "importations#export", :defaults => { :format => 'csv' } #Importação arquivo .csv
   devise_for :users
-  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  mount RailsAdmin::Engine => '/', as: 'rails_admin'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.htm
 end
