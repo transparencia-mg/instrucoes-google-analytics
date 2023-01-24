982d4eef21ef3ed93a47674aae281720d4632ace
Gabriel Dornas <gabrielbdornas@gmail.com>
2023-01-24 13:18:54 -0300

    Update UrlClassification migration

--- db/migrate/20230124161514_create_url_classifications.rb
+++ db/migrate/20230124161514_create_url_classifications.rb
@@ -1,6 +1,8 @@
 class CreateUrlClassifications < ActiveRecord::Migration[6.1]
   def change
     create_table :url_classifications do |t|
+      t.string :url
+      t.string :classification
 
       t.timestamps
     end
